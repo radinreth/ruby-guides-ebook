868b0cf2a1e764ff75393b508a5a5ecc08c95573
Anton Zhidkov <anton.v.zhidkov@gmail.com>
2015-01-26 11:19:31 +0300

    Added Active Record Basics guide.

---
+++ src/Models/Active Record Basics/build_active_record_basics.bat
@@ -0,0 +1,4 @@
+..\..\..\tools\Pandoc\pandoc.exe --css "..\..\styles\tables.css" -o active_record_basics.epub active_record_basics.md
+..\..\..\tools\calibre\ebook-convert.exe active_record_basics.epub active_record_basics.mobi
+..\..\..\tools\calibre\ebook-convert.exe active_record_basics.epub active_record_basics.pdf
+pause
