7654385bb00c3e14a8a97fb8cdb5cf3314f502dd
azhidkov <anton.v.zhidkov@gmail.com>
2015-01-29 13:46:52 +0300

    removed all files

--- src/Models/Active Record Associations/active_record_associations.bat
+++
@@ -1,7 +0,0 @@
-..\..\..\toolsPandocpandoc.exe --css '..\..\..\styles\tables.css' -o active_record_associations.epub active_record_associations.md
-
-..\..\..\tools\calibre\ebook-convert.exe active_record_associations.epub active_record_associations.mobi
-
-..\..\..\tools\calibre\ebook-convert.exe active_record_associations.epub active_record_associations.pdf
-
-pause
--- src/Models/Active Record Basics/build_active_record_basics.bat
+++
@@ -1,4 +0,0 @@
-..\..\..\tools\Pandoc\pandoc.exe --css "..\..\styles\tables.css" -o active_record_basics.epub active_record_basics.md
-..\..\..\tools\calibre\ebook-convert.exe active_record_basics.epub active_record_basics.mobi
-..\..\..\tools\calibre\ebook-convert.exe active_record_basics.epub active_record_basics.pdf
-pause
